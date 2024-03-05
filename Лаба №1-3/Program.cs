471acbc47cc29320f0332751622d6fc8077e6f16
arizona26 <rublevskijmaksim00@gmail.com>
2024-03-05 20:24:36 +0200

    Add project files.

---
+++ Лаба №1-3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введiть D = ");
+int D = int.Parse(Console.ReadLine()); 
+List<int> A = new List<int> { 121, 324, 467, 124, 783, 978, 125, 654, 143 };
+int result = A.FirstOrDefault(num => num > 0 && num % 10 == D);
+Console.WriteLine("Результат: " + (result != 0 ? result.ToString() : "0"));
+
+Console.ReadLine();
